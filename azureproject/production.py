402ebe56975227c50930f78e031fe76d5e1a7dfa
r-yamada-0216 <125527375+r-yamada-0216@users.noreply.github.com>
2023-02-20 16:31:42 +0900

    test

--- azureproject/production.py
+++ azureproject/production.py
@@ -13,7 +13,7 @@ conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.s
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=conn_str_params['user'],
-    # dbpass=conn_str_params['password'],
+    dbpass=conn_str_params['password'],
     dbhost=conn_str_params['host'],
     dbname=conn_str_params['dbname']
 )
