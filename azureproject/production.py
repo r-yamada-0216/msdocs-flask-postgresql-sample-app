5c8af005bad58c46bb644d66eb0fccdf16ce8d5f
r-yamada-0216 <125527375+r-yamada-0216@users.noreply.github.com>
2023-02-20 10:49:34 +0900

    mod db info

--- azureproject/production.py
+++ azureproject/production.py
@@ -12,12 +12,8 @@ conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['postgresadmin'],
-    dbpass=conn_str_params['P@ssw0rd123456789'],
-    dbhost=conn_str_params['postgresql-test-230216.postgres.database.azure.com'],
-    dbname=conn_str_params['postgresql-test-230216']
-    # dbuser=conn_str_params['postgresadmin'],
-    # dbpass=conn_str_params['P@ssw0rd123456789'],
-    # dbhost=conn_str_params['postgresql-test-230216.postgres.database.azure.com'],
-    # dbname=conn_str_params['postgresql-test-230216']
+    dbuser=conn_str_params['user'],
+    dbpass=conn_str_params['password'],
+    dbhost=conn_str_params['host'],
+    dbname=conn_str_params['dbname']
 )
